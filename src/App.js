166a62e41b7dd2710583c540fe968634cccb4962
benkay <benjamin.node@gmail.com>
2019-10-14 05:26:39 +0100

    Finished building Qupload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import { SignIn } from './components/Auth'
+import ROUTES from './routes'
+import {withAuthentication} from './components/Session'
+import Creator, { CreatorWorks } from './components/Creator'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Navigation />
+    <Route path={ROUTES.SIGNIN} exact component={SignIn} />
+    <Route path={ROUTES.CREATOR} exact component={Creator} />
+    <Route path={ROUTES.CREATORWORKS} exact component={CreatorWorks} />
+  </Router>
+)
 
-export default App;
+export default withAuthentication(App)
